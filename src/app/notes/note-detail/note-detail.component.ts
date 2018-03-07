759cc7feb317ed04bd4fc29c406eec8bed01c91b
ARubiato <alessanderrubiato@yahoo.com>
2018-03-08 05:00:33 +0800

    new set of files added for google and facebook auth

---
+++ src/app/notes/note-detail/note-detail.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input } from '@angular/core';
+import { NoteService } from '../note.service';
+import { Note } from '../note-model';
+import { Time } from '@angular/common';
+import { timestamp } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'note-detail',
+  templateUrl: './note-detail.component.html',
+  styleUrls: ['./note-detail.component.scss'],
+})
+export class NoteDetailComponent {
+
+  @Input()
+  note: Note;
+
+  constructor(private noteService: NoteService) { }
+
+  addHeartToNote(val: number) {
+    if (this.note.id) {
+      const date = new Date().getTime();
+      this.noteService.updateNote(this.note.id, { hearts: val + 1 });
+    } else {
+      console.error('Note missing ID!');
+    }
+  }
+
+  addDislikeToNote(val: number) {
+    if (this.note.id) {
+      this.noteService.updateNote(this.note.id, { dislike: val + 1 });
+    } else {
+      console.error('Note missing ID!');
+    }
+  }
+
+  deleteNote(id: string) {
+    this.noteService.deleteNote(id);
+  }
+
+  // timeNote(time: Time) {
+  //   this.noteService.timeNote(Time);
+  // }
+
+
+
+}
